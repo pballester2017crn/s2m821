865eb258ea70b9fef76c567e16c7464461c728a6
Pasqual Ballester Paradis <pballester@artsgrafiques.org>
2021-02-05 15:09:22 +0100

    php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>Variable</h1>
+	</body>
+</html>
